1d08b83f045fdeb5febf05d75800b20b46689603
LuisMathias20 <luiscraft15@gmail.com>
2021-05-23 03:17:57 -0300

    Subir inicio projeto

---
+++ src/main/java/org/casadocodigo/loja/config/AppWebConfiguration.java
@@ -0,0 +1,11 @@
+package org.casadocodigo.loja.config;
+
+import org.casadocodigo.loja.controllers.HomeController;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@ComponentScan(basePackageClasses={HomeController.class})
+public class AppWebConfiguration {
+
+}
